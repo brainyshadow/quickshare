9ae170959272610a99f7f82a7f179d045f30faae
brainyshadow <seb997a@hotmail.com>
2022-08-24 18:55:52 -0400

    Worked on Ui
    Started working on the welcome page's UI.

--- website/quickshare/src/pages/Welcome.tsx
+++ website/quickshare/src/pages/Welcome.tsx
@@ -3,9 +3,14 @@ import logo from "./logo.svg";
 
 function Welcome() {
   return (
-    <div className="bg-fuchsia-300 content-center">
-      <div>
+    <div className="grid grid-cols-2 gap-4 bg-fuchsia-300 content-center ">
+      <div className="h-screen flex">
+        <div className="m-auto">
+          <h1 className="text-9xl font-sans mb-10">quickshare</h1>
+          <h2 className="mt-10">The easiest to share text between two devices</h2>
+        </div>
       </div>
+      <div></div>
     </div>
   );
 }
