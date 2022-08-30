49119301970051e8abb39bb8c7ab05a2bf0bcba5
brainyshadow <seb997a@hotmail.com>
2022-08-30 12:42:00 -0400

    Worked on the UI
    Worked the UI for the welcome page trying to make it more inviting.

--- website/quickshare/src/pages/Welcome.tsx
+++ website/quickshare/src/pages/Welcome.tsx
@@ -4,7 +4,7 @@ import QRCode from "react-qr-code";
 
 function Welcome() {
   return (
-    <div className="grid grid-cols-3 gap-4 bg-fuchsia-300 content-center ">
+    <div className="xl:grid xl:grid-cols-3 xl:gap-4 xl:content-center bg-fuchsia-300 ">
       <div className="col-span-2 h-screen flex">
         <div className="m-auto">
           <h1 className="text-9xl font-sans mb-10">quickshare</h1>
@@ -13,17 +13,20 @@ function Welcome() {
           </h2>
         </div>
       </div>
-      <div className="m-auto">
-        <button
-          type="button"
-          className="text-black bg-white hover:ring-2 focus:ring-blue-300 font-medium rounded-md text-lg px-5 py-2.5 text-center mr-2 mb-2 dark:bg-blue-600 dark:focus:ring-blue-800"
-        >
-          Create
-        </button>
-        <div>
-
+      <div className="grid h-screen place-items-center">
+        <div className="grid grid-rows-6">
+          <div className="w-full">
+            <button
+              type="button"
+              className="text-black bg-white hover:ring-2 font-medium rounded-md text-lg px-5 py-2.5 text-center mr-2 mb-2 dark:bg-blue-600 dark:focus:ring-blue-800 w-full"
+            >
+              Create
+            </button>
+          </div>
+          <div className="row-span-5">
+            <QRCode value="https://quickshare.com/" size={300} />
+          </div>
         </div>
-        <QRCode value="https://quickshare.com/" size={300} />
       </div>
     </div>
   );
