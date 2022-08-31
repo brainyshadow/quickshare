accb9cebe17ee0de8e549700379952ee809d1239
brainyshadow <seb997a@hotmail.com>
2022-08-31 11:52:23 -0400

    Worked on UI
    Worked the welcome page adding links and a couple of other styling changes.

--- website/quickshare/src/pages/Welcome.tsx
+++ website/quickshare/src/pages/Welcome.tsx
@@ -4,26 +4,31 @@ import QRCode from "react-qr-code";
 
 function Welcome() {
   return (
-    <div className="xl:grid xl:grid-cols-3 xl:gap-4 xl:content-center bg-fuchsia-300 ">
-      <div className="col-span-2 h-screen flex">
+    <div className="lg:grid lg:grid-cols-3 lg:gap-4 xl:content-center bg-fuchsia-300 ">
+      <div className="col-span-2 lg:h-screen flex">
         <div className="m-auto">
           <h1 className="text-9xl font-sans mb-10">quickshare</h1>
           <h2 className="mt-10">
-            The easiest to share text between two devices
+            The easiest to share text between two devices.
+            <br />
+            <a href="https://github.com/brainyshadow/quickshare">Open source</a>
+            . Simple. Easy.
           </h2>
         </div>
       </div>
-      <div className="grid h-screen place-items-center">
-        <div className="grid grid-rows-6">
-          <div className="w-full">
-            <button
-              type="button"
-              className="text-black bg-white hover:ring-2 font-medium rounded-md text-lg px-5 py-2.5 text-center mr-2 mb-2 dark:bg-blue-600 dark:focus:ring-blue-800 w-full"
-            >
-              Create
-            </button>
+      <div className="lg:grid lg:h-screen lg:place-items-center">
+        <div className="lg:grid lg:grid-rows-6 md:flex">
+          <div className="lg:w-full">
+            <a href="/edit">
+              <button
+                type="button"
+                className="text-black bg-white hover:ring-2 font-medium rounded-md text-lg px-5 py-2.5 text-center mr-2 mb-2 dark:bg-blue-600 dark:focus:ring-blue-800 lg:w-full"
+              >
+                Create
+              </button>
+            </a>
           </div>
-          <div className="row-span-5">
+          <div className="lg:row-span-5 md:flex">
             <QRCode value="https://quickshare.com/" size={300} />
           </div>
         </div>
