07e26018a18afc394b13a00bf4f845a6051b7f6d
brainyshadow <seb997a@hotmail.com>
2022-10-05 17:54:55 -0400

    Added white background to text editor

--- website/quickshare/src/components/TextEditor.tsx
+++ website/quickshare/src/components/TextEditor.tsx
@@ -1,14 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import {Editor, EditorState} from 'draft-js';
-import 'draft-js/dist/Draft.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Editor, EditorState } from "draft-js";
+import "draft-js/dist/Draft.css";
 
 function TextEditor() {
-  const [editorState, setEditorState] = React.useState(
-    () => EditorState.createEmpty(),
+  const [editorState, setEditorState] = React.useState(() =>
+    EditorState.createEmpty()
   );
 
-  return <Editor editorState={editorState} onChange={setEditorState} />;
+  return (
+    <>
+      <div className="bg-white">
+        <Editor editorState={editorState} onChange={setEditorState} />
+      </div>
+    </>
+  );
 }
 
 export default TextEditor;
