06068a1fcdca284a276a398d9f9a0825ef0efb41
brainyshadow <seb997a@hotmail.com>
2022-10-22 22:15:00 -0400

    Started createDocument function
    Started the create document function that will eventually return the document id and other data required to create a new quickshare link.

--- functions/functions/src/index.ts
+++ functions/functions/src/index.ts
@@ -1,9 +1,19 @@
 import * as functions from "firebase-functions";
+const admin = require("firebase-admin");
 
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
 //
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+admin.initializeApp();
+
+export const createDocument = functions.https.onRequest(
+  async (request, response) => {
+    const documentId: string = "00000000";
+    var query = await admin.firestore().collection("dev").get();
+    query.forEach((doc: any) => {
+      console.log(doc.id);
+    });
+    functions.logger.info("Hello logs!", { structuredData: true });
+    response.send(documentId);
+  }
+);
