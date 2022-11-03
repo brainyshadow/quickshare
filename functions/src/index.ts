73b1a4152bb714ec4399973d7047a300da7a568c
brainyshadow <seb997a@hotmail.com>
2022-11-03 12:13:58 -0400

    Updated function

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -9,9 +9,13 @@ admin.initializeApp();
 export const createDocument = functions.https.onRequest(
   async (request, response) => {
     const documentId: string = "00000000";
-    var query = await admin.firestore().collection("dev").get();
+    var query = await admin
+      .firestore()
+      .collection("dev")
+      .where("expiryTime", "<", new Date())
+      .get();
     query.forEach((doc: any) => {
-      console.log(doc.id);
+      functions.logger.info(doc.id);
     });
     functions.logger.info("Hello logs!", { structuredData: true });
     response.send(documentId);
