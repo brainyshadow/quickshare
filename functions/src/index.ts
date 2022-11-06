28383ef3fe07ba27c928fcd1c9186c51286d5986
brainyshadow <seb997a@hotmail.com>
2022-11-06 10:55:11 -0500

    Updated functions
    Updated functions to add error handling

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -4,13 +4,13 @@ const admin = require("firebase-admin");
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
 //
-const enviroment = functions.config().enviroment;
+const enviroment = functions.config().config.enviroment;
 
 admin.initializeApp();
-console.log(enviroment)
+console.log(enviroment);
 export const createDocument = functions.https.onRequest(
   async (request, response) => {
-
+    let documentId: string = "";
     var query = await admin
       .firestore()
       .collection(enviroment)
@@ -21,22 +21,32 @@ export const createDocument = functions.https.onRequest(
     query.forEach((doc: any) => {
       documents.push(doc);
     });
-    admin
-      .firestore()
-      .collection(enviroment)
-      .doc(documents[0].id)
-      .set({
-        data: "",
-        expiryTime: new Date(),
-      })
-      .then(() => {
-        functions.logger.info("Document successfully written!");
-      })
-      .catch((error: any) => {
-        functions.logger.info("Error writing document: ", error);
-      });
 
-    functions.logger.info("Documents Found", { documents });
-    response.send(documents[0].id);
+    documentId = typeof documents[0]?.id === "string" ? documents[0]?.id : "";
+
+    console.log(documentId);
+    if (documentId === "") {
+      functions.logger.error("No Documents Found", { documents });
+      response.status(500).send("No Documents Found");
+    } else {
+      admin
+        .firestore()
+        .collection(enviroment)
+        .doc(documents[0].id)
+        .set({
+          data: "",
+          expiryTime: new Date(),
+        })
+        .then(() => {
+          functions.logger.info("Document successfully written!");
+          functions.logger.info("Documents Found", { documents });
+          response.send(documentId);
+        })
+        .catch((error: any) => {
+          functions.logger.info("Error writing document: ", error);
+          functions.logger.info("Error Clearing Document", { error });
+          response.status(500).send("");
+        });
+    }
   }
 );
