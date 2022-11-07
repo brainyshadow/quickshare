70784d9acc3f4c81caee84a86f47e5548ddc9ebb
brainyshadow <seb997a@hotmail.com>
2022-11-07 18:11:33 -0500

    Updated eslint

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -9,42 +9,42 @@ const enviroment = functions.config().config.enviroment;
 admin.initializeApp();
 console.log(enviroment);
 export const createDocument = functions.https.onRequest(
-  async (request, response) => {
-    let documentId: string = "";
-    var query = await admin
-      .firestore()
-      .collection(enviroment)
-      .where("expiryTime", "<", new Date())
-      .limit(1)
-      .get();
-    let documents: Array<any> = [];
-    query.forEach((doc: any) => {
-      documents.push(doc);
-    });
-    documentId = typeof documents[0]?.id === "string" ? documents[0]?.id : "";
-    if (documentId === "") {
-      functions.logger.error("No Documents Found", { documents });
-      response.status(500).send("No Documents Found");
-    } else {
-      const currentTime: Date = new Date();
-      currentTime.setMinutes(currentTime.getMinutes() + 10);
-      admin
-        .firestore()
-        .collection(enviroment)
-        .doc(documents[0].id)
-        .set({
-          data: "",
-          expiryTime: currentTime,
-        })
-        .then(() => {
-          response.send(documentId);
-          functions.logger.info("Document successfully written!");
-        })
-        .catch((error: any) => {
-          functions.logger.info("Error writing document: ", error);
-          functions.logger.info("Error Clearing Document", { error });
-          response.status(500).send("");
-        });
+    async (request, response) => {
+      let documentId = "";
+      const query = await admin
+          .firestore()
+          .collection(enviroment)
+          .where("expiryTime", "<", new Date())
+          .limit(1)
+          .get();
+      const documents: Array<any> = [];
+      query.forEach((doc: any) => {
+        documents.push(doc);
+      });
+      documentId = typeof documents[0]?.id === "string" ? documents[0]?.id : "";
+      if (documentId === "") {
+        functions.logger.error("No Documents Found", {documents});
+        response.status(500).send("No Documents Found");
+      } else {
+        const currentTime: Date = new Date();
+        currentTime.setMinutes(currentTime.getMinutes() + 10);
+        admin
+            .firestore()
+            .collection(enviroment)
+            .doc(documents[0].id)
+            .set({
+              data: "",
+              expiryTime: currentTime,
+            })
+            .then(() => {
+              response.send(documentId);
+              functions.logger.info("Document successfully written!");
+            })
+            .catch((error: any) => {
+              functions.logger.info("Error writing document: ", error);
+              functions.logger.info("Error Clearing Document", {error});
+              response.status(500).send("");
+            });
+      }
     }
-  }
 );
